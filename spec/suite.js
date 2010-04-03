fbd38483841d091d796c3137f9479e197fdf9eba
Yohei Sasaki <yssk22@gmail.com>
2010-04-04 01:59:22 +0900

    [mod] jspec time added.

--- spec/suite.js
+++ spec/suite.js
@@ -3,10 +3,10 @@ load("spec/jspec.js");
 load("spec/couchdb-server-main.js");
 
 (function(){
-	JSpec.include({
-		name: 'JUnit',
-		formatters: {
-			JUnitXml: function(results, options) {
+   JSpec.include({
+      name: 'JUnit',
+      formatters: {
+         JUnitXml: function(results, options) {
             function e(s){
                return s.toString().replace(/&/g, "&amp;")
                   .replace(/\"/g, "&quot;")
@@ -14,36 +14,43 @@ load("spec/couchdb-server-main.js");
                   .replace(/</g, "&lt;")
                   .replace(/>/g, "&gt;");
             }
-				var w = new java.io.FileWriter('jspec.xml');
-				w.write('<?xml version="1.0" encoding="UTF-8"?>\n');
-				w.write('<testsuites>\n');
-				JSpec.each(results.allSuites, function(suite) {
-					var attribs = {
-						name: suite.description,
-						tests: suite.specs.length,
-						assertions: 0,
-						failures: 0,
-						specs: 0};
-					var content = JSpec.inject(suite.specs, '', function(content, spec) {
-						attribs.assertions += spec.assertions.length;
-						attribs.failures += spec.passed() ? 0 : 1;
-						attribs.specs += 1;
-						return content +'    <testcase name="'+ e(spec.description) +'" assertions="'+spec.assertions.length+'"/>\n';
-					});
-					w.write('  <testsuite');
-					for (var key in attribs) {
-						w.write(' ' + key + '="' + e(attribs[key]) + '"');
-					}
-					w.write('>\n');
-					w.write(content);
-					w.write('  </testsuite>\n');
-				});
-				w.write('</testsuites>\n');
-				w.close();
-				quit(results.stats.failures);
-			}
-		}
-	});
+            var w = new java.io.FileWriter('jspec.xml');
+            w.write('<?xml version="1.0" encoding="UTF-8"?>\n');
+            w.write('<testsuites>\n');
+            JSpec.each(results.allSuites, function(suite) {
+               var attribs = {
+                  name: suite.description,
+                  tests: suite.specs.length,
+                  assertions: 0,
+                  failures: 0,
+                  specs: 0,
+                  time: 0
+               };
+               var content = JSpec.inject(suite.specs, '', function(content, spec) {
+                  attribs.assertions += spec.assertions.length;
+                  attribs.failures += spec.passed() ? 0 : 1;
+                  attribs.specs += 1;
+                  attribs.time += spec.time || 0;
+                  return content +'    <testcase '
+                     + 'name="'+ e(spec.description) +'" '
+                     + 'assertions="'+spec.assertions.length + '" '
+                     + 'time="' + spec.time + '"'
+                     + '/>\n';
+               });
+               w.write('  <testsuite');
+               for (var key in attribs) {
+                  w.write(' ' + key + '="' + e(attribs[key]) + '"');
+               }
+               w.write('>\n');
+               w.write(content);
+               w.write('  </testsuite>\n');
+            });
+            w.write('</testsuites>\n');
+            w.close();
+            quit(results.stats.failures);
+         }
+      }
+   });
 })();
 
 JSpec.addMatchers({
